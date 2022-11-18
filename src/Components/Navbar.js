46bd9e7657e2827a49de671ae24f5e0664f16b1a
rdwebdev24 <rohitdhakad101@gmail.com>
2022-11-18 15:45:08 +0530

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,19 +3,14 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg ">
+  <div className="container-fluid  ">
     <a className="navbar-brand" href="#">Mausam Suhana <i className="fa fa-cloud" aria-hidden="true"></i></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav me-auto  mb-lg-0">
         <li className="nav-item">
           <a className="nav-link" href="https://rdwebdev24.vercel.app/" target="_blank">Visit rdwebdev <i className="fa fa-external-link" aria-hidden="true"></i></a>
         </li>
       </ul>
-    </div>
   </div>
 </nav>
     </>
